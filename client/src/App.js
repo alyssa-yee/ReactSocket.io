dc34530a702839afc99f7e8fe88b0f2aaab6efcc
alyssa-yee <alyssayee99@gmail.com>
2023-04-05 14:48:06 -0700

    websocket update

--- client/src/App.js
+++ client/src/App.js
@@ -3,45 +3,13 @@ import './App.css';
 import Navbar from "./components/Navbar.js"
 import PlayerList from './components/PlayerList.js';
 import Message from './components/Message';
-import io from  'socket.io-client';
-import {useEffect, useState} from 'react';
-
-// cd server npm start to get the server running
-// cd client/my-react-app -> npm start to get the page running
-
-
-const socket = io.connect("http://localhost:3001")
 
 function App() {
-  const [message, setMessage] = useState("");
-  const [messageReceived, setMessageReceived] = useState("");
-  const sendMessage = () =>{
-    socket.emit("send_message", {message});
-  };
-
-  useEffect(() =>{
-    socket.on("receive_message", (data) =>{
-      // alert(data.message);
-      setMessageReceived(data.message);
-    });
-  });
-
-
   return (
     <div className="App">
-
-      <input placeholder='Message...' onChange ={(event) => {
-        setMessage(event.target.value);
-      }}/>
-
-      <button onClick = {sendMessage}>Send Message</button>
-      <h1>Message: </h1>
-      {messageReceived}
-
       <Navbar />
       <PlayerList />
       <Message />
-
     </div>
   );
 }
--- client/src/components/Message.js
+++ client/src/components/Message.js
@@ -1,13 +1,35 @@
 import React from 'react';
+import io from  'socket.io-client';
+import {useEffect, useState} from 'react';
+
+const socket = io.connect("http://localhost:3001")
 
 export default function Message() {
+
+    const [message, setMessage] = useState("");
+    const [messageReceived, setMessageReceived] = useState("");
+    const sendMessage = () =>{
+      socket.emit("send_message", {message});
+    };
+  
+    useEffect(() =>{
+      socket.on("receive_message", (data) =>{
+        // alert(data.message);
+        setMessageReceived(data.message);
+      });
+    });
+
     return (
         <div className = 'Message-Div'>
             <div className='conversation-div'>
-                
+                    <input placeholder='Message...' onChange ={(event) => {
+                        setMessage(event.target.value);
+                    }}/>
+                    <button onClick = {sendMessage}>Send Message</button>
+                    <h1>Message: </h1>
+                    {messageReceived}
             </div>
-            <div className='typemessage-div'>
-            </div>
+            <div className='typemessage-div'></div>
         </div>
     )
 }
